0650f217da2c89f932e830267e325f24b7b5d199
Michael Katic <michaelkatic@gmail.com>
2019-06-20 16:28:57 -0700

    Initial functional readme component with edit/save button and saving/loading capabilities (no save/load ui yet).

---
+++ Editor/ReadmeEditor.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace TP.Readme {
+    [CustomEditor(typeof(Readme)), ExecuteInEditMode]
+    public class ReadmeEditor : Editor
+    {
+        private bool editing = false;
+        private Readme readme;
+        private GUIStyle selectableText;
+        private GUIStyle editableText;
+        private int textPadding = 5;
+        
+        public override void OnInspectorGUI()
+        {
+            readme = (Readme) target;
+            bool empty = readme.Text == "";
+            
+            selectableText = new GUIStyle();
+            selectableText.wordWrap = true;
+            selectableText.padding = new RectOffset(textPadding, textPadding, textPadding + 2, textPadding);
+            
+            editableText = new GUIStyle(GUI.skin.textArea);
+            editableText.padding = new RectOffset(textPadding, textPadding, textPadding, textPadding);
+    
+            float textAreaWidth = EditorGUIUtility.currentViewWidth - 19;
+            float textAreaHeight = editableText.CalcHeight(new GUIContent(readme.Text), textAreaWidth);
+            
+            EditorGUILayout.Space();
+            
+            if (!editing)
+            {
+                if (empty)
+                {
+                    EditorGUILayout.HelpBox("Click edit to add your readme!", MessageType.Info);
+                }
+                else
+                {
+                    EditorGUILayout.SelectableLabel(readme.Text, selectableText, GUILayout.Height(textAreaHeight + 4));
+                }
+            }
+            else
+            {
+                readme.Text = EditorGUILayout.TextArea(readme.Text, editableText, GUILayout.Height(textAreaHeight));
+    
+                Undo.RecordObject(target, "Readme");
+            }
+            
+            EditorGUILayout.Space();
+    
+            if (!editing)
+            {
+                if (GUILayout.Button("Edit"))
+                {
+                    editing = true;
+                }
+            }
+            else
+            {
+                if (GUILayout.Button("Save"))
+                {
+                    editing = false;
+                }
+            }
+        }
+    }
+}
---
+++ Readme.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace TP.Readme {
+    [Serializable]
+    public class ReadmeData
+    {
+        public string text;
+    }
+    
+    [ExecuteInEditMode]
+    public class Readme : MonoBehaviour
+    {   
+        [SerializeField] private ReadmeData readmeData;
+        private string lastSavedFileName = "";
+    
+        public string Text
+        {
+            get { return readmeData.text; }
+            set
+            {
+                if (value == null)
+                {
+                    readmeData.text = "";
+                }
+                else
+                {
+                    readmeData.text = value;
+                }
+            }
+        }
+        
+        public void Save()
+        {
+            string jsonReadMeData = JsonUtility.ToJson(readmeData, true);
+            string fileName = Application.persistentDataPath + "/Readme_" + gameObject.name + "_" + DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss") + ".json";
+            File.WriteAllText (fileName, jsonReadMeData);
+            lastSavedFileName = fileName;
+            Debug.Log("Readme RichText saved to file: " + fileName);
+        }
+    
+        public void LoadLastSave()
+        {
+            if (lastSavedFileName != "")
+            {
+                string fileToLoad = lastSavedFileName;
+                
+                //Save before loading just in case!
+                Save();
+                
+                string json = File.ReadAllText(fileToLoad);
+                Text = JsonUtility.FromJson<ReadmeData>(json).text;
+            }
+        }
+    }
+}
