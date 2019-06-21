63bcd3f6460b93eff16b4ae712489a2c961476fa
Michael Katic <michaelkatic@gmail.com>
2019-06-20 17:23:55 -0700

    Added toggle buttons for bold and italic. Added font style and size selection. Added debug info to advances options. Added keyboard shortcuts for bold/italic. They don't seem to work in the latest unity.

--- Readme.cs
+++ Readme.cs
@@ -48,6 +48,7 @@ namespace TP.Readme {
                 
                 text = MakePoorText(readmeData.richText);
                 BuildRichTextTagMap();
+                RebuildStyleMaps();
             }
         }
     
@@ -84,6 +85,98 @@ namespace TP.Readme {
             
             //  <color=#00ffffff>
         }
+
+        public bool IsStyle(string style, int index)
+        {
+            bool isStyle = false;
+
+            if (StyleMaps.ContainsKey(style))
+            {
+                List<bool> styleMap = StyleMaps[style];
+                if (styleMap.Count > index)
+                {
+                    isStyle =  styleMap[index];
+                }
+            }
+
+            return isStyle;
+        }
+        
+        public void ToggleStyle(string tag, int startIndex, int length)
+        {
+            RebuildStyleMaps();
+            
+            if (!supportedTags.Contains(tag))
+            {
+                Debug.LogWarning("The <" + tag + "> tag is not supported");
+            }
+            
+            if (!StyleMaps.ContainsKey(tag))
+            {
+                styleMaps.Add(tag, Enumerable.Repeat(false, text.Length).ToList());
+            }
+            
+            List<bool> styleMap = StyleMaps[tag];
+            
+            try
+            {
+                bool styleFound = false;
+                bool nonStyleFound = false;
+                foreach (bool isStyle in styleMap.GetRange(startIndex, length))
+                {
+                    styleFound |= isStyle;
+                    nonStyleFound |= !isStyle;
+                }
+    
+                bool styleMixed = styleFound && nonStyleFound;
+                bool forceAllStyle = styleMixed;
+    
+                styleMaps[tag] = styleMap.Select((currentIsStyle, index) => {
+                    bool newIsStyle = currentIsStyle;
+                    if (index >= startIndex && index < startIndex + length)
+                    {
+                        newIsStyle = forceAllStyle || !currentIsStyle;
+                    }
+                    return newIsStyle;
+                }).ToList();
+                
+                ApplyStyleMap(tag);
+            }
+            catch (Exception exception)
+            {
+                Debug.LogError(exception);
+                LoadLastSave();
+            }
+        }
+    
+        public void ApplyStyleMap(string tag)
+        {
+            List<bool> styleMapCopy = StyleMaps[tag];
+            
+            RichText = RichText
+                .Replace(Tag(tag), "")
+                .Replace(EndTag(tag), "");
+    
+            // Iterate backwards through bold map so we don't have to rebuild richTextTagMap every time
+            // we insert a new tag.
+            string newRichText = RichText;
+            for (int i = styleMapCopy.Count; i >= 0; i--)
+            {
+                bool currentIsStyle = i < styleMapCopy.Count ? styleMapCopy[i] : false;
+                bool nextIsStyle = i > 0 ? styleMapCopy[i - 1] : false;
+    
+                if (!currentIsStyle && nextIsStyle)
+                {
+                    newRichText = newRichText.Insert(GetRichIndex(i), EndTag(tag));
+                }
+                else if (currentIsStyle && !nextIsStyle)
+                {
+                    newRichText = newRichText.Insert(GetRichIndex(i), Tag(tag));
+                }
+            }
+    
+            RichText = newRichText;
+        }
     
         public void BuildRichTextTagMap()
         {
@@ -113,6 +206,44 @@ namespace TP.Readme {
                     }
                 }
             }
+        }
+        
+        public void RebuildStyleMaps()
+        {
+            Dictionary<string, List<bool>> updatedStyleMaps = new Dictionary<string, List<bool>>();
+            
+            foreach (string tag in supportedTags)
+            {
+                List<bool> styleMap = Enumerable.Repeat(false, text.Length).ToList();
+    
+                int lastTagIndex = 0;
+                int maxTagCount = RichText.Split('<').Length - 1;
+                for (int i = 0; i < maxTagCount; i++)
+                {
+                    int startOfStyle = RichText.IndexOf(Tag(tag), lastTagIndex);
+                    if (startOfStyle == -1)
+                    {
+                        break;
+                    }
+                    lastTagIndex = startOfStyle + 1;
+    
+                    int endOfStyle = RichText.IndexOf(EndTag(tag), startOfStyle);
+                    if (endOfStyle == -1)
+                    {
+                        break;
+                    }
+                
+                    for (int styleIndex = startOfStyle; styleIndex < endOfStyle; styleIndex++)
+                    {
+                        int poorTextIndex = GetPoorIndex(styleIndex);
+                        styleMap[poorTextIndex] = true;
+                    }
+                }
+    
+                updatedStyleMaps[tag] = styleMap;
+            }
+    
+            styleMaps = updatedStyleMaps;
         }
         
         //TODO can optimize by building a rich to poor index map.
@@ -167,6 +298,32 @@ namespace TP.Readme {
                 string json = File.ReadAllText(fileToLoad);
                 RichText = JsonUtility.FromJson<ReadmeData>(json).richText;
             }
+        }
+    
+        public bool IsStyle(string tag, int index, bool richIndex = false)
+        {
+            bool isStyle = false;
+            if (richIndex)
+            {
+                index = GetPoorIndex(index);
+            }
+    
+            if (StyleMaps.ContainsKey(tag))
+            {
+                isStyle = StyleMaps[tag][index];
+            }
+            
+            return isStyle;
+        }
+        
+        public string Tag(string tagName)
+        {
+            return "<" + tagName + ">";
+        }
+        
+        public string EndTag(string tagName)
+        {
+            return "</" + tagName + ">";
         }
     }
 }
